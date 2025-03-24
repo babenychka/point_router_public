fc4d85624189d278f8f956f46f05bdddaa9086c3
babenychka <mikro_0@mail.ru>
2025-03-24 23:14:55 +0400

    fix initVM

--- initVM.sh
+++ initVM.sh
@@ -1,10 +1,17 @@
+#!/bin/bash
+
 apt update
 apt install -y docker-compose python3.12-venv net-tools
 
-bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+wget https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh
+/bin/bash install.sh
+rm install.sh
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
+sudo usermod -aG docker $USER
+newgrp docker
+sudo systemctl start docker
 
 git clone https://github.com/babenychka/point_router_public.git
 cd /root/point_router_public/sing-box 
@@ -12,28 +19,15 @@ docker-compose up -d
 cd /root/point_router_public/sing-box-vless
 docker-compose up -d
 
-cd /root/
+cd /root/point_router_public/
+# закинуть проект в гитхаб и не ебать себе мозги, просто пулить и все
+# python3.12 -m venv connectionsLimiter
+# cd /root/connectionsLimiter
+# source bin/activate
+# pip install requests schedule
+# deactivate
 
-python3.12 -m venv connectionsLimiter
-cd /root/connectionsLimiter
-source bin/activate
-pip install requests schedule
-deactivate
-cp /root/point_router_public/connectionsLimiter.py /root/connectionsLimiter/main.py
-
-cat << 'EOF' > /root/connectionsLimiter/run.sh
-#!/bin/bash
-cd /root/connectionsLimiter
-source /root/connectionsLimiter/bin/activate
-
-echo "Starting main.py" >&2
-/root/connectionsLimiter/bin/python -u main.py 2>&1
-echo "Finished main.py" >&2
-echo "Finished main.py" >&2
-
-deactivate
-EOF
-chmod +x /root/connectionsLimiter/run.sh
+chmod +x /root/point_router_public/connectionsLimiter/run.sh
 
 cat << 'EOF' > /etc/systemd/system/connectionsLimiter.service
 [Unit]
@@ -42,7 +36,7 @@ After=network.target
 
 [Service]
 Type=simple
-ExecStart=/root/connectionsLimiter/run.sh
+ExecStart=/root/point_router_public/connectionsLimiter/run.sh
 Restart=on-failure
 User=root
 StandardOutput=journal
@@ -55,5 +49,15 @@ EOF
 systemctl daemon-reload
 systemctl enable connectionsLimiter.service
 systemctl start connectionsLimiter.service
+
+# install and configure nginx hc srv
+
+apt install nginx
+sed -i '/server_name _;/a\
+        location \/hc\/ {\
+                return 200 "OK";\
+        }' /etc/nginx/sites-enabled/default
+nginx -t && nginx -s reload
+
 
 echo PLEASE get confix x-ui 
